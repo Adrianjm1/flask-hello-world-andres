beab04195684153fe041d93a69a72979d41423c3
Adrian Molina <72955302+Adrianjm1@users.noreply.github.com>
2024-10-28 13:07:24 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import pgeocode
+
 app = Flask(__name__)
-@app.route('/')
 
 config = {
     "DEBUG": False,          # some Flask specific configs
@@ -9,19 +10,15 @@ config = {
 }
 app.config.from_mapping(config)
 
-def query_location():
-    location = request.args.get('location')
-    top_k = request.args.get('top_k', default=3, type=int)
-    if not location:
-        return jsonify({"error": "Location parameter is required"}), 400
-    result = nomi.query_location(location, top_k=top_k)
-    result_dict = result.to_dict(orient='records')
-    return jsonify(result_dict)
-
-
+@app.route('/')
 def home():
     return 'Hello, mi pana!'
+
 @app.route('/about')
-@app.route('/query_location', methods=['GET'])
 def about():
     return 'About'
+
+@app.route('/query_location', methods=['GET'])
+def query_location():
+
+    return 'prueba'
