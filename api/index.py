78d024619bfd4863f0195df8475d5940bd467586
Adrian Molina <72955302+Adrianjm1@users.noreply.github.com>
2024-10-28 13:24:46 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -10,10 +10,14 @@ def about():
     return 'About'
 
 
+nomi = pgeocode.Nominatim('us')
+
 @app.route('/query_location', methods=['GET'])
 def query_location():
     location = request.args.get('location')
     top_k = request.args.get('top_k', default=3, type=int)
     if not location:
         return jsonify({"error": "Location parameter is required"}), 400
+    result = nomi.query_location(location, top_k=top_k)
+    result_dict = result.to_dict(orient='records')
     return 'Hello, mi pana crunch!'
