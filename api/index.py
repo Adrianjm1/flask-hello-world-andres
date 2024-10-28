b4f031aac2fd339378b2603c1dd364a81b0a066d
Adrian Molina <72955302+Adrianjm1@users.noreply.github.com>
2024-10-28 13:22:56 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -12,4 +12,8 @@ def about():
 
 @app.route('/query_location', methods=['GET'])
 def query_location():
+    location = request.args.get('location')
+    top_k = request.args.get('top_k', default=3, type=int)
+    if not location:
+        return jsonify({"error": "Location parameter is required"}), 400
     return 'Hello, mi pana crunch!'
