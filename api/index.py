2a3b5d0565462f0411051b71e7ca8037424ab088
Adrian Molina <72955302+Adrianjm1@users.noreply.github.com>
2024-10-28 11:56:07 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,33 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import pgeocode
+from flask_caching import Cache
+
+config = {
+    "DEBUG": False,          # some Flask specific configs
+    "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
+    "CACHE_DEFAULT_TIMEOUT": 300
+}
 
 app = Flask(__name__)
+# tell Flask to use the above defined config
+app.config.from_mapping(config)
+cache = Cache(app)
+nomi = pgeocode.Nominatim('us')
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+@app.route('/query_location', methods=['GET'])
+@cache.cached(response_hit_indication=True)
+@cache.memoize()
+def query_location():
+    location = request.args.get('location')
+    top_k = request.args.get('top_k', default=3, type=int)
+    if not location:
+        return jsonify({"error": "Location parameter is required"}), 400
+    result = nomi.query_location(location, top_k=top_k)
+    result_dict = result.to_dict(orient='records')
+    return jsonify(result_dict)
 
-@app.route('/about')
-def about():
-    return 'About'
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
